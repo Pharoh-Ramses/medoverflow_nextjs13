e60079b6242d88026b328961f0275247878cb1ea
Ramses Angles <ramses.angles@skiff.com>
2024-02-02 16:02:54 -0600

    eslint shenanegans

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,6 +10,13 @@ import { getQuestions } from "@/lib/actions/question.action";
 import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Home",
+  description:
+    "Ask health questions, get direct licensed answers, extending the patient provider relationship, one question at a time.",
+};
+
 export default async function Home({ searchParams }: SearchParamsProps) {
   const result = await getQuestions({
     searchQuery: searchParams.q,
