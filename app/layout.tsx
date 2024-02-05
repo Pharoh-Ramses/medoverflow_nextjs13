d7840740dd3ca0cd9281e1d27accdfe145e0955f
Ramses Angles <ramses.angles@skiff.com>
2024-02-05 16:45:33 -0600

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 import "../styles/prism.css";
 import { ThemeProvider } from "@/context/ThemeProvider";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -44,6 +45,7 @@ export default function RootLayout({
           }}
         >
           <ThemeProvider>{children}</ThemeProvider>
+          <SpeedInsights />
           <Analytics />
         </ClerkProvider>
       </body>
