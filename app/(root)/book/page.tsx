c341a8592552a909c0f62dcafba07e190dc53b4f
Ramses Angles <ramses.angles@skiff.com>
2024-02-15 22:56:07 -0600

    working on the amelia api booking, successfully fetched timeslots

--- app/(root)/book/page.tsx
+++ app/(root)/book/page.tsx
@@ -1,18 +1,32 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { getTimeSlots } from "../../../lib/actions/booking.action";
 
 const page = () => {
-    const service = '1';
-    const duration = '3600';
-    const persons = '1';
-    const startDate = '2022-02-08';
+  const service = "1";
+  const duration = "3600";
+  const persons = "1";
+  const startDate = "2022-02-20";
+
+  const handleGetTimeSlots = async () => {
+    try {
+      const timeSlots = await getTimeSlots({
+        service,
+        duration,
+        persons,
+        startDate,
+      });
+      console.log(timeSlots);
+    } catch {
+      console.log("error");
+    }
+  };
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900 flex">Coming Soon...</h1>
-            <Button onClick={() => getTimeSlots({service, duration, persons, startDate})}>Get Time Slots</Button>
+      <Button onClick={handleGetTimeSlots}>Get Time Slots</Button>
     </div>
   );
 };
--- lib/actions/booking.action.ts
+++ lib/actions/booking.action.ts
@@ -4,21 +4,22 @@ import { NextResponse } from "next/server";
 import { GetTimeSlotsParams } from "./shared.types";
 
 export async function getTimeSlots(params: GetTimeSlotsParams) {
-    const { service, duration, persons, startDate } = params;
-    try {
-        const timeSlotsResponse = await fetch(`https://shop.rumehealth.com/wp-admin/admin-ajax.php?action=wpamelia_api&call=/api/v1/slots&serviceId=${service}&serviceDuration=${duration}&persons=${persons}&startDate=${startDate}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Amelia ${process.env.AMELIA_API_KEY}`,
-            },
-
-        }
-        );
-        const timeSlots = await timeSlotsResponse.json();
-        console.log(timeSlots);
-        return JSON.stringify({timeSlots});
-    } catch (error:any) {
-        return NextResponse.json({ error: error.message });
-    }
+  const { service, duration, persons, startDate } = params;
+  try {
+    const timeSlotsResponse = await fetch(
+      `https://shop.rumehealth.com/wp-admin/admin-ajax.php?action=wpamelia_api&call=/api/v1/slots&serviceId=${service}&serviceDuration=${duration}&persons=${persons}&startDateTime=${startDate}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Amelia: `${process.env.AMELIA_API_KEY}`,
+        },
+      }
+    );
+    const timeSlots = await timeSlotsResponse.json();
+    console.log(timeSlots);
+    return timeSlots;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message });
+  }
 }
